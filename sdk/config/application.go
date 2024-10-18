c0a874a30a098b73d2e7dc5948ce90316ec62d32
nieyinliang <nieyinliang@wanjiedata.com>
2024-10-18 14:30:29 +0800

    optimize application config

--- sdk/config/application.go
+++ sdk/config/application.go
@@ -1,14 +1,16 @@
 package config
 
 type Application struct {
-	ReadTimeout   int
-	WriterTimeout int
-	Host          string
-	Port          int64
-	Name          string
-	Mode          string
-	DemoMsg       string
-	EnableDP      bool
+	ReadTimeout       int
+	WriterTimeout     int
+	Host              string
+	Port              int64
+	Name              string
+	Mode              string
+	DemoMsg           string
+	EnableDP          bool
+	EnableSwaggerPage bool `json:"enableSwaggerPage" yaml:"enableSwaggerPage"`
+	EnableFormPage    bool `json:"enableFormPage" yaml:"enableFormPage"`
 }
 
 var ApplicationConfig = new(Application)
