dbe3669487c67c85406d3eed0110a41aadb7474f
nyl1001 <602645105@qq.com>
2025-05-24 14:15:06 +0800

    optimize claims

--- sdk/pkg/jwtauth/claims.go
+++ sdk/pkg/jwtauth/claims.go
@@ -60,7 +60,7 @@ func (m MapClaims) String(key string) string {
 	case string:
 		return value.(string)
 	default:
-		fmt.Errorf("maptoclaims key to string invalid value '%v' type '%T' key %v", value, value, key)
+		fmt.Printf("maptoclaims key to string invalid value '%v' type '%T' key %v", value, value, key)
 		return ""
 	}
 }
@@ -117,4 +117,164 @@ func (m MapClaims) UInt64(key string) (uint64, error) {
 	default:
 		return 0, fmt.Errorf("invalid value '%v' type '%T'", value, value)
 	}
+}
+
+// Int64Array try to convert to []int64 by key
+func (m MapClaims) Int64Array(key string) ([]int64, error) {
+	raw := m[key]
+	if raw == nil {
+		return nil, fmt.Errorf("invalid key '%v'", key)
+	}
+
+	raw, ok := m[key]
+	if !ok {
+		return nil, fmt.Errorf("invalid key '%v'", key)
+	}
+
+	rawList, ok := raw.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("error format, key '%v'", key)
+	}
+
+	var vals []int64
+	for _, r := range rawList {
+		switch r.(type) {
+		case json.Number:
+			intVal, err := r.(json.Number).Int64()
+			if err != nil {
+				return nil, err
+			}
+			vals = append(vals, intVal)
+		case float64:
+			vals = append(vals, int64(r.(float64)))
+		case string:
+			intVal, err := strconv.ParseInt(r.(string), 10, 0)
+			if err != nil {
+				return nil, err
+			}
+			vals = append(vals, intVal)
+		default:
+			return nil, fmt.Errorf("invalid value '%v' type '%T'", r, r)
+		}
+	}
+
+	return vals, nil
+}
+
+// StringArray try to convert to []string by key
+func (m MapClaims) StringArray(key string) []string {
+	raw := m[key]
+	if raw == nil {
+		return nil
+	}
+
+	raw, ok := m[key]
+	if !ok {
+		return nil
+	}
+
+	rawList, ok := raw.([]interface{})
+	if !ok {
+		return nil
+	}
+
+	var vals []string
+	for _, r := range rawList {
+		switch r.(type) {
+		case json.Number:
+			vals = append(vals, r.(json.Number).String())
+		case float64:
+			vals = append(vals, strconv.FormatFloat(r.(float64), 'g', -1, 64))
+		case string:
+			vals = append(vals, r.(string))
+		default:
+			fmt.Printf("maptoclaims key to string invalid value '%v' type '%T' key %v", r, r, key)
+			vals = append(vals, "")
+		}
+	}
+	return vals
+}
+
+// IntArray try to convert to []int by key
+func (m MapClaims) IntArray(key string) ([]int, error) {
+	raw := m[key]
+	if raw == nil {
+		return nil, fmt.Errorf("invalid key '%v'", key)
+	}
+
+	raw, ok := m[key]
+	if !ok {
+		return nil, fmt.Errorf("invalid key '%v'", key)
+	}
+
+	rawList, ok := raw.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("error format, key '%v'", key)
+	}
+
+	var vals []int
+	for _, r := range rawList {
+		switch r.(type) {
+		case json.Number:
+			intVal, err := r.(json.Number).Int64()
+			if err != nil {
+				return nil, err
+			}
+			vals = append(vals, int(intVal))
+		case float64:
+			vals = append(vals, int(r.(float64)))
+		case string:
+			intVal, err := strconv.Atoi(r.(string))
+			if err != nil {
+				return nil, err
+			}
+			vals = append(vals, intVal)
+		default:
+			return nil, fmt.Errorf("invalid value '%v' type '%T'", r, r)
+		}
+	}
+
+	return vals, nil
+}
+
+// UInt64Array try to convert to []uint64 by key
+func (m MapClaims) UInt64Array(key string) ([]uint64, error) {
+	raw := m[key]
+	if raw == nil {
+		return nil, fmt.Errorf("invalid key '%v'", key)
+	}
+
+	raw, ok := m[key]
+	if !ok {
+		return nil, fmt.Errorf("invalid key '%v'", key)
+	}
+
+	rawList, ok := raw.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("error format, key '%v'", key)
+	}
+
+	var vals []uint64
+	for _, r := range rawList {
+		switch r.(type) {
+		case json.Number:
+			intVal, err := r.(json.Number).Int64()
+			if err != nil {
+				return nil, err
+			}
+			vals = append(vals, uint64(intVal))
+		case float64:
+			vals = append(vals, uint64(r.(float64)))
+		case string:
+			intVal, err := strconv.Atoi(r.(string))
+			if err != nil {
+				return nil, err
+			}
+			vals = append(vals, uint64(intVal))
+		default:
+			return nil, fmt.Errorf("invalid value '%v' type '%T'", r, r)
+		}
+	}
+
+	return vals, nil
 }
