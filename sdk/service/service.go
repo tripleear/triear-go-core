f6b091775914985464a5fdfe6404a2551a30d7a0
nyl1001 <602645105@qq.com>
2025-05-30 17:26:19 +0800

    service support db transaction

--- sdk/service/service.go
+++ sdk/service/service.go
@@ -25,3 +25,23 @@ func (db *Service) AddError(err error) error {
 	}
 	return db.Error
 }
+
+func (db *Service) RunInTransaction(fn func(tx *gorm.DB) error) error {
+	// 开始事务
+	return db.Orm.Transaction(func(tx *gorm.DB) error {
+		// 捕获 panic，确保即使发生 panic 也能回滚事务
+		defer func() {
+			if r := recover(); r != nil {
+				tx.Rollback() // panic 时回滚
+				panic(r)      // 继续抛出 panic
+			}
+		}()
+
+		// 执行传入的操作
+		err := fn(tx)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+}
