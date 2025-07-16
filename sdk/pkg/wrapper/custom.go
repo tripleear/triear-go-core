ed02ef5b2ace68e0ab292d17c2317f39d6d8b8e7
nyl1001 <602645105@qq.com>
2025-07-16 20:30:58 +0800

    add custom func to redis wrapper

--- sdk/pkg/wrapper/custom.go
+++ sdk/pkg/wrapper/custom.go
@@ -10,3 +10,19 @@ func (cli *RedisClient) HSetWithPrefixInPipeline(ctx context.Context, p redis.Pi
 	fullKey := cachePrefix + key
 	return p.HSet(ctx, fullKey, values...)
 }
+
+func (cli *RedisClient) HGetWithPrefixInPipeline(ctx context.Context, p redis.Pipeliner, key string, field string) *redis.StringCmd {
+	cachePrefix := GetCachePrefixFromContext(ctx)
+	fullKey := cachePrefix + key
+	return p.HGet(ctx, fullKey, field)
+}
+func (cli *RedisClient) HGetAllWithPrefixInPipeline(ctx context.Context, p redis.Pipeliner, key string) *redis.MapStringStringCmd {
+	cachePrefix := GetCachePrefixFromContext(ctx)
+	fullKey := cachePrefix + key
+	return p.HGetAll(ctx, fullKey)
+}
+
+func (cli *RedisClient) DelWithPrefixInPipeline(ctx context.Context, p redis.Pipeliner, keys ...string) *redis.IntCmd {
+	fullKeys := addPrefixToKeys(ctx, keys)
+	return p.Del(ctx, fullKeys...)
+}
