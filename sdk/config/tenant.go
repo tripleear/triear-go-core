63b7a51740f66d124f679d09b306a2dccc00a270
nyl1001 <602645105@qq.com>
2025-09-11 14:36:30 +0800

    add tenant config

---
+++ sdk/config/tenant.go
@@ -0,0 +1,11 @@
+package config
+
+type HostTenants struct {
+	Tenants map[string]*Tenant `yaml:"tenants"`
+	Db      *Database          `yaml:"db"`
+}
+
+type Tenant struct {
+	Id  uint64 `yaml:"id"`
+	Key string `yaml:"key"`
+}
